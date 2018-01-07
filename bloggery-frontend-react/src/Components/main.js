6de0d7024d6e3025cda47aa1766c81f481e029be
Mehtavarun <mehtavarun96@rediffmail.com>
2018-01-07 19:16:53 +0530

    Animations Added
    Animations added on home page

--- bloggery-frontend-react/src/Components/main.js
+++ bloggery-frontend-react/src/Components/main.js
@@ -4,17 +4,15 @@ import './css/main.css';
 import "animate.css/animate.min.css";
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import ArrowUpward from 'material-ui/svg-icons/navigation/arrow-upward';
-import IconButton from 'material-ui/IconButton';
+import ArrowDownward from 'material-ui/svg-icons/navigation/arrow-downward';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 import PropTypes from 'prop-types';
 import {deepPurple50} from 'material-ui/styles/colors';
-
-const style={
-	marginTop: '200px',
-	marginLeft: '650px',
-	scrollBehavior: 'smooth',
-	animationDelay:'2s'
-}
+import ScrollAnimation from 'react-animate-on-scroll';
+import FlatButton from 'material-ui/FlatButton';
+import image3 from './images/3.jpg';
+import image1 from './images/1.jpg';
+import image2 from './images/2.jpg'
 
 export default class Main extends React.Component{
 
@@ -22,26 +20,139 @@ export default class Main extends React.Component{
                 return { muiTheme: getMuiTheme(baseTheme) };
             }
 
-    handleScroll(){
-    	window.scrollBy(0,100);
-    }
-
 	render(){
 		return(
 			<div className = "wrapper">
-				<div className = "top animated fadeInDown"  ref={node => this.node = node}>
-				 	<h2 className="animated bounce">start blog today.</h2>
-				 	<button id = "getStartedButton" className=" animated fadeInLeft">Get Started</button><br/>
-				 	<button id = "signInButton" className=" animated fadeInRight">Sign In</button><br/>
-				 	<IconButton className="animated infinite bounce" style={style} onClick={this.handleScroll.bind(this)}><ArrowUpward color={deepPurple50} /></IconButton>
-				</div>
-				<div className = 'middle animated fadeInUp'>
-					<p>sdfnsajd</p><hr/><p>pkldflkan</p>
-				</div>
+				<FloatingActionButton className="animated slideInUp" style={style} 
+					backgroundColor="orange">
+			 			<ArrowDownward color={deepPurple50} />
+			 	</FloatingActionButton>
+			 	<Top />
+				<Mid />
 			</div>
       )
 	}
 }
+
+class Top extends React.Component{
+
+	constructor(props){
+		super();
+	}
+	render(){
+		return(
+			<div className = "top animated fadeInDown" >
+			 	<h2 className = "animated fadeInDown">start blog today.</h2>
+			 	<p className = "topText animated fadeIn">you will never forget experience with<br/> bloggery...</p>
+			 	<FlatButton 
+			 		id = "getStartedButton" 
+			 		backgroundColor= '#a4c639' 
+			 		hoverColor="#8AA62F" 
+			 		className = "animated fadeInLeft" 
+			 		style={styleButton[0]}>
+			 			Get Started
+			 	</FlatButton><br/>
+			 	<FlatButton 
+			 		id = "signInButton" 
+			 		backgroundColor= '#3d5afe' 
+			 		hoverColor="#304FFE"
+			 		className = "animated fadeInRight"
+			 		style={styleButton[1]}>
+			 			Sign In
+			 	</FlatButton><br/>
+			 	
+			</div>
+		)
+	}
+}
+
+class Mid extends React.Component{
+
+	constructor(props){
+		super();
+	}
+
+	render(){
+		return(
+			<div>
+			<div className = 'middle1'>
+				<ScrollAnimation animateIn="fadeInLeft" >
+					<p className='firstBlock'>Turns out that when you make it
+					easy to create interesting things, 
+					that’s exactly what people do. All
+					those great, random blogs your friends
+					send you, those are Bloggery. We’ll
+					help you find and follow blogs like that.
+					</p>
+				</ScrollAnimation>
+				<ScrollAnimation animateIn="fadeInRight">
+					<img height='260' src={image3} />
+				</ScrollAnimation>
+			</div>
+			<div className = 'middle2'>
+				<ScrollAnimation animateIn="fadeInLeft">
+					<img height='300' src={image2} />
+				</ScrollAnimation>
+				<ScrollAnimation animateIn="fadeInRight" >
+				<p className='firstBlock'>
+					Turns out that when you make it
+					easy to create interesting things, 
+					that’s exactly what people do. All
+					those great, random blogs your friends
+					send you, those are Bloggery. We’ll
+					help you find and follow blogs like that.
+					</p>
+				</ScrollAnimation>
+			</div>
+			<div className = 'middle3'>
+				<ScrollAnimation animateIn="fadeInRight" >
+				<p className='firstBlock'>
+					Turns out that when you make it
+					easy to create interesting things, 
+					that’s exactly what people do. All
+					those great, random blogs your friends
+					send you, those are Bloggery. We’ll
+					help you find and follow blogs like that.
+					</p>
+				</ScrollAnimation>
+
+				<ScrollAnimation animateIn="fadeInLeft">
+					<img height='300' src={image1} />
+				</ScrollAnimation>
+			</div>
+			</div>
+		)
+	}
+}
+
+
+const style={
+	scrollBehavior: 'smooth',
+	animationDelay:'1s',
+	position:'fixed',
+	left:'1270px',
+	top:'595px'
+}
+const styleButton=[
+	{  
+		paddingLeft: '89px',
+		paddingRight: '80px',
+		height: '50px',
+		marginLeft: '540px',
+		marginTop: '0px',
+		color: 'white',
+		animationDelay: '1s'},
+	{
+		paddingLeft: '80px',
+		height: '50px',
+		paddingRight: '80px',
+		marginLeft: '540px',
+		marginTop: '10px',
+		color: 'white',
+		animationDelay: '1s',
+		paddingLeft: '100px',
+		paddingRight: '100px'}
+]
 
 Main.childContextTypes = {
             muiTheme: PropTypes.object.isRequired,
